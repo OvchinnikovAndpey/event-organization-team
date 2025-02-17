e315b79e139f1434b0e55d249ec3fba528e9745c
Andrey Ovchinnikov <andpey.ovchinnikov@yandex.ru>
2025-02-17 21:45:14 +0300

    изменил размерность модалки, поднастроил бургер, поправил описание команды

--- src/components/Pages/Gallerey/Gallerey.tsx
+++ src/components/Pages/Gallerey/Gallerey.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Gallerey.module.css";
 import { GallereyData } from "./GallereyData";
+import Modal from "../../Modal/Modal";
 
 const ImageGrid: React.FC = () => {
+  const [modalActive, setModalActive] = useState(false);
+  const [selectedImage, setSelectedImage] = useState<string>("");
+
+  const handleImageClick = (imageSrc: string) => {
+    setSelectedImage(imageSrc);
+    setModalActive(true);
+  };
+
   return (
     <div className={styles.card__block}>
       <h1 className={styles.title}>Галлерея фотографий с мероприятий</h1>
@@ -14,9 +23,18 @@ const ImageGrid: React.FC = () => {
             alt={image.description}
             loading="lazy"
             className={styles.section__image}
+            onClick={() => handleImageClick(image.thumbnail)}
           />
         ))}
       </div>
+
+      <Modal active={modalActive} setActive={setModalActive}>
+        <img 
+          src={selectedImage} 
+          alt="Selected" 
+          className={styles.modalImage}
+        />
+      </Modal>
     </div>
   );
 };
